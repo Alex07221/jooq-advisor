ece4230452e8a5b3a2ca8aff3c27ca0298a8f548
xuzebiao <Qq52178945>
2021-09-14 16:22:12 +0800

    add support method

--- src/main/java/cn/yxtreme/jooq/utils/Streams.java
+++ src/main/java/cn/yxtreme/jooq/utils/Streams.java
@@ -2,6 +2,7 @@ package cn.yxtreme.jooq.utils;
 
 import java.util.List;
 import java.util.Map;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -60,6 +61,56 @@ public class Streams {
      */
     public static final <E, R, U, T> Map<U, List<T>> streamGroup(Stream<E> e, Function<Stream<E>, Stream<R>> function, Function<R, U> classifier, Function<R, T> classifier2) {
         return function.apply(e).collect(Collectors.groupingBy(classifier::apply, Collectors.mapping(classifier2, Collectors.toList())));
+    }
+
+    /**
+     * stream语法糖，分组
+     *
+     * @param e           需要处理的stream
+     * @param function    stream的处理
+     * @param classifier  group的主键
+     * @param classifier2 group的值
+     * @return 分组成功的结果
+     */
+    public static final <E, R, U, T> Map<U, List<T>> streamGroupByProperties(Stream<E> e, Function<E, R> function, Function<R, U> classifier, Function<R, T> classifier2) {
+        return e.map(function).collect(Collectors.groupingBy(classifier::apply, Collectors.mapping(classifier2, Collectors.toList())));
+    }
+
+    /**
+     * stream语法糖，分组
+     *
+     * @param e           需要处理的stream
+     * @param function    stream的处理
+     * @param classifier  group的主键
+     * @param classifier2 group的值
+     * @return 分组成功的结果
+     */
+    public static final <E, R, U, T> Map<U, List<T>> streamGroupByProperties(Stream<E> e, Class<T> cla, BiFunction<E, Class<T>, R> function, Function<R, U> classifier, Function<R, T> classifier2) {
+        return e.map(t -> function.apply(t, cla)).collect(Collectors.groupingBy(classifier::apply, Collectors.mapping(classifier2, Collectors.toList())));
+    }
+
+    /**
+     * stream语法糖，分组
+     *
+     * @param e          需要处理的stream
+     * @param function   stream的处理
+     * @param classifier group的主键
+     * @return 分组成功的结果
+     */
+    public static final <E, R, U> Map<U, List<R>> streamGroupByProperties(Stream<E> e, Function<E, R> function, Function<R, U> classifier) {
+        return e.map(function).collect(Collectors.groupingBy(classifier::apply));
+    }
+
+    /**
+     * stream语法糖，分组
+     *
+     * @param e          需要处理的stream
+     * @param function   stream的处理
+     * @param classifier group的主键
+     * @return 分组成功的结果
+     */
+    public static final <E, R, U> Map<U, List<R>> streamGroupByProperties(Stream<E> e, Class<R> cla, BiFunction<E, Class<R>, R> function, Function<R, U> classifier) {
+        return e.map(t -> function.apply(t, cla)).collect(Collectors.groupingBy(classifier::apply));
     }
 
 }
